20e2ae6a3de6b07c4f0ed05b5a675d8586935303
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2026-09-26 11:25:23 +0600

    Refactor Technology component to utilize useSettings for dynamic glass properties and improve LiquidGlass usage

--- components/Pages/Home/Technology.tsx
+++ components/Pages/Home/Technology.tsx
@@ -1,4 +1,6 @@
+"use client";
 import LiquidGlass from "@/components/Resuable/LiquideGlass/LiquideGlass";
+import { useSettings } from "@/context/SettingsContext";
 import React from "react";
 const technologies = [
   { img: "/technology/pngwing.com (1).png", name: "HTML" },
@@ -13,23 +15,35 @@ const technologies = [
   { img: "/technology/pngwing.com (8).png", name: "Express.js" },
 ];
 export default function Technology() {
+  const {
+    glassFrost,
+    glassTint,
+  } = useSettings();
   return (
     <div className="mt-20">
       <h1 className="text-lg font-bold tracking-wide mb-4 text-white">
         My Tech Stack
       </h1>
-      <LiquidGlass className="p-6">
+      <LiquidGlass className="p-6" radius={16} tint={glassTint} frost={glassFrost}>
         <div className="flex flex-wrap justify-between gap-5">
           {technologies.map((technology) => (
             <div>
-              <LiquidGlass key={technology.name} className="p-2 w-fit">
+              <LiquidGlass
+                key={technology.name}
+                className="p-2 w-fit"
+                radius={16}
+                tint={glassTint}
+                frost={glassFrost}
+              >
                 <img
                   src={technology.img}
                   alt={technology.name}
                   className="h-16 w-16 object-contain"
                 />
               </LiquidGlass>
-              <p className="text-center mt-2 text-white font-semibold tracking-wide">{technology.name}</p>
+              <p className="text-center mt-2 text-white font-semibold tracking-wide">
+                {technology.name}
+              </p>
             </div>
           ))}
         </div>
